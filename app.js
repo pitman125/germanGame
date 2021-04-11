573453e6334b8e0bda78342a05a65cbe5f341610
pitman125 <the_pitman125@live.co.uk>
2021-04-11 14:28:17 +0100

    added linter and express server

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/start', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.put('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/continue', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
